7a961dbbaf13b99ee3cada350c48baee8f37307a
cwn <1014949353@qq.com>
2018-11-29 15:30:26 +0800

    修改了封装库文件

--- source_files/MYTableReuseId/UITableViewCell+ReuseID.m
+++ source_files/MYTableReuseId/UITableViewCell+ReuseID.m
@@ -8,28 +8,30 @@
 
 #import "UITableViewCell+ReuseID.h"
 @implementation UITableViewCell (ReuseID)
-+ (__kindof UITableViewCell *)cellFromXibWithTableView:(UITableView *)tableView IndexPath:(NSIndexPath *)indexPath
++ (__kindof UITableViewCell *)getCellFromXibWithTableView:(UITableView *)tableView IndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([self class])];
+    NSString *identifier = [NSStringFromClass([self class]) componentsSeparatedByString:@"."].lastObject;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if(cell){
         return cell;
     }
-    UINib *nib = [UINib nibWithNibName:NSStringFromClass([self class]) bundle:[NSBundle mainBundle]];
-    [tableView registerNib:nib forCellReuseIdentifier:NSStringFromClass([self class])];
+    UINib *nib = [UINib nibWithNibName:identifier bundle:[NSBundle mainBundle]];
+    [tableView registerNib:nib forCellReuseIdentifier:identifier];
     //    return [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([self class])];
-    UITableViewCell * cell2 = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([self class]) forIndexPath:indexPath];
+    UITableViewCell * cell2 = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
     return cell2;//height for row
 }
-+ (__kindof UITableViewCell *)cellFromClassWithTableView:(UITableView *)tableView IndexPath:(NSIndexPath *)indexPath
++ (__kindof UITableViewCell *)getCellFromClassWithTableView:(UITableView *)tableView IndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([self class])];
+    NSString *identifier = [NSStringFromClass([self class]) componentsSeparatedByString:@"."].lastObject;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if(cell)
         return cell;
-    [tableView registerClass:[self class] forCellReuseIdentifier:NSStringFromClass([self class])];
-    return [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([self class]) forIndexPath:indexPath];
+    [tableView registerClass:[self class] forCellReuseIdentifier:identifier];
+    return [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
 }
 
-+ (__kindof UITableViewCell *)cellLoadNibNamed:(NSString *)name WithTableView:(UITableView *)tableView identifier:(NSString *)identifier{
++ (__kindof UITableViewCell *)getCellLoadNibNamed:(NSString *)name WithTableView:(UITableView *)tableView identifier:(NSString *)identifier{
     id cell = [tableView dequeueReusableCellWithIdentifier:identifier];
     if (cell == nil) {
         NSArray *nibs = [[NSBundle mainBundle]loadNibNamed:name owner:self options:nil];
@@ -51,21 +53,23 @@
 @end
 @implementation UITableViewHeaderFooterView (ReuseID)
 
-+ (__kindof UITableViewHeaderFooterView *)HeaderFooterViewFromXibWithTableView:(UITableView *)tableView{
-    UITableViewHeaderFooterView * HFView = [tableView dequeueReusableHeaderFooterViewWithIdentifier:NSStringFromClass([self class])];
++ (__kindof UITableViewHeaderFooterView *)getHeaderFooterViewFromXibWithTableView:(UITableView *)tableView{
+    NSString *identifier = [NSStringFromClass([self class]) componentsSeparatedByString:@"."].lastObject;
+    UITableViewHeaderFooterView * HFView = [tableView dequeueReusableHeaderFooterViewWithIdentifier:identifier];
     if(HFView)
         return HFView;
-    UINib *nib = [UINib nibWithNibName:NSStringFromClass([self class]) bundle:[NSBundle mainBundle]];
-    [tableView registerNib:nib forHeaderFooterViewReuseIdentifier:NSStringFromClass([self class])];
-    return [tableView dequeueReusableHeaderFooterViewWithIdentifier:NSStringFromClass([self class])];
+    UINib *nib = [UINib nibWithNibName:identifier bundle:[NSBundle mainBundle]];
+    [tableView registerNib:nib forHeaderFooterViewReuseIdentifier:identifier];
+    return [tableView dequeueReusableHeaderFooterViewWithIdentifier:identifier];
 }
 /** 适用于class创建  */
-+ (__kindof UITableViewHeaderFooterView *)HeaderFooterViewFromClassWithTableView:(UITableView *)tableView {
-    UITableViewHeaderFooterView * HFView = [tableView dequeueReusableHeaderFooterViewWithIdentifier:NSStringFromClass([self class])];
++ (__kindof UITableViewHeaderFooterView *)getHeaderFooterViewFromClassWithTableView:(UITableView *)tableView {
+    NSString *identifier = [NSStringFromClass([self class]) componentsSeparatedByString:@"."].lastObject;
+    UITableViewHeaderFooterView * HFView = [tableView dequeueReusableHeaderFooterViewWithIdentifier:identifier];
     if(HFView)
         return HFView;
-    [tableView registerClass:[self class] forHeaderFooterViewReuseIdentifier:NSStringFromClass([self class])];
-    return [tableView dequeueReusableHeaderFooterViewWithIdentifier:NSStringFromClass([self class])];
+    [tableView registerClass:[self class] forHeaderFooterViewReuseIdentifier:identifier];
+    return [tableView dequeueReusableHeaderFooterViewWithIdentifier:identifier];
 }
 
 - (void)setBackgroundColor:(UIColor *)backgroundColor{
